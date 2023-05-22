7c323f5d1be9beb9485b2df11e528c5c654cd700
Joshua Tucker <jtucker@elevenfifty.org>
2023-05-22 12:02:37 -0400

    Added HttpClient GetAsync

--- IntroToApi.ConsoleApp/Program.cs
+++ IntroToApi.ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# class that can send HTTP requests
+HttpClient client = new HttpClient();
+
+// The base URL for Star Wars API
+string baseUrl = "https://swapi.dev/api";
+
+// First GET request to get people/1 endpoint
+// GetAsync method is async meaning we need the await keyword
+HttpResponseMessage response = await client.GetAsync($"{baseUrl}/people/1");
+Console.WriteLine(response.IsSuccessStatusCode);
+
+if (response.IsSuccessStatusCode)
+{
+    string responseContent = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseContent);
+}
+
